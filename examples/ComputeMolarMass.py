d9ee03c293babf555425ff6e387f067ca0dbc09b
Yang-Junjie <1973690778@qq.com>
2025-10-10 18:16:25 +0800

    feat:实现计算相对分子质量，添加开源证书

---
+++ examples/ComputeMolarMass.py
@@ -0,0 +1,5 @@
+﻿from ChemToolbox.core.relative_molecular_mass import relative_molecular_mass
+
+print(f"水分子的摩尔质量{relative_molecular_mass("H_2O"):.4f}") 
+
+print(f"硫酸根离子的摩尔质量{relative_molecular_mass("SO_4^{2-}"):.4f}")
---
+++ examples/FindElement.py
@@ -0,0 +1,3 @@
+﻿from ChemToolbox.utils import find_element
+
+print(find_element.find_element("Cu"))
